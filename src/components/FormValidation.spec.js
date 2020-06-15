41fadb6a4be93653bb202e28b456cb4a1d4393f7
Aleksandra Miazek <ola.miazek@gmail.com>
2020-06-15 14:52:54 +0200

    start adding tests

---
+++ src/components/FormValidation.spec.js
@@ -0,0 +1,7 @@
+//import FormValidation from './FormValidation.vue' //Test suite failed to run, Cannot find module 'babel-core'Require stack:
+
+describe('FormValidation.vue', () => {
+    test('setup correctly', () => {
+        expect(true).toBe(true)
+    })
+})
